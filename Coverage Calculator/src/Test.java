53b7efd988d3c1edbb1de73802b0fa9905b75b60
Mark Rutledge <rutledgemarkr@gmail.com>
2013-04-04 22:23:40 -0400

    Signed-off-by: Mark Rutledge <rutledgemarkr@gmail.com>

--- Coverage Calculator/src/Test.java
+++ Coverage Calculator/src/Test.java
@@ -4,5 +4,6 @@ import javax.swing.JOptionPane;
 public class Test {
 	public static void main(String args[]) {
 		JOptionPane.showMessageDialog(null, "THIS IS NEAT!...if it works.", "Yo", JOptionPane.QUESTION_MESSAGE);
+		JOptionPane.showMessageDialog(null, "YUP. THIS IS NEAT!", "YO", JOptionPane.WARNING_MESSAGE);
 	}
 }
