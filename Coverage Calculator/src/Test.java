c49c19fe281deb51f3435e1388fb1bbee54a5e14
p-mack <pmcannally@radford.edu>
2013-04-04 23:50:20 -0300

    Update Test.java

--- Coverage Calculator/src/Test.java
+++ Coverage Calculator/src/Test.java
@@ -3,6 +3,6 @@ import javax.swing.JOptionPane;
 
 public class Test {
 	public static void main(String args[]) {
-		JOptionPane.showMessageDialog(null, "THIS IS NEAT!...if it works.", "Yo", JOptionPane.QUESTION_MESSAGE);
+		JOptionPane.showMessageDialog(null, "THIS IS NEAT!...if it works.", "Yo", "What?", JOptionPane.QUESTION_MESSAGE);
 	}
 }
